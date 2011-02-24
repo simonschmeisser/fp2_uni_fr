c6050db7a502b1474285b9f049a9e4891600896e
Simon Schmeisser <mail_to_wrt@gmx.de>
2011-02-24 22:27:48 +0100

    auswertung++

---
+++ Positronium/Auswertung/rechnungen.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+from math import *
+
+#Zufällige Koinzidenzen
+n_sz2 = 197906 / 100.0
+n_sz3 = 188591 / 100.0
+tau = 3.38e-9*10
+
+print "Zufällige Koinzidenzen: ", tau*n_sz2*n_sz3
+
+
