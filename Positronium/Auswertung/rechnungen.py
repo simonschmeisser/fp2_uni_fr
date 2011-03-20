8931c3342da65075bf57a1eddd4de9f8aabb3ba2
Simon Schmeisser <mail_to_wrt@gmx.de>
2011-03-21 00:07:06 +0100

    Und noch ein paar Kleinigkeiten ...

--- Positronium/Auswertung/rechnungen.py
+++ Positronium/Auswertung/rechnungen.py
@@ -16,5 +16,21 @@ print "90 Grad:", 167.0/100, " pm ", sqrt(167.0)/100
 
 print "falsches Delay: ", 19./1000, " pm ", sqrt(19.0)/1000
 
+print "Fehlerrechnung erste Energieeichung"
+
+def print_errors(x01275, x01275_err, x0511, x0511_err):
+  xscale = (1275-511)/(x01275-x0511)
+  xscale_error = xscale/(x01275-x0511)*sqrt(x01275_err**2+x0511_err**2)
+  print "xscale: %.4g \\pm %.3g" %(xscale, xscale_error)
 
 
+  xoffset = 511-x0511*xscale
+  xoffset_error = sqrt( (xscale*x0511_err)**2 + (xscale_error*x0511)**2 )
+  print "xoffset= %.3g \\pm %.3g" %( xoffset, xoffset_error)
+
+print "SZ1: "
+print_errors(199.2, 0.1, 72.4, 0.1)
+print "SZ2: "
+print_errors(233., 0.03, 83.1, 0.1)
+print "SZ3: "
+print_errors(268.4, 0.1, 99.1, 0.1)
